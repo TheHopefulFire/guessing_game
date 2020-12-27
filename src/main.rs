19be32d552f25dba6b2c7be11a9820bdb633b0c2
Jade Lynn Masker <jadelynn@masker.family>
2020-12-26 23:56:41 -0500

    implement guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::Write;
+use rand::Rng;
+
+fn main()
+{
+    let guessing_range = 1..101;
+    let secret_number = rand::thread_rng().gen_range(guessing_range.clone());
+
+    loop
+    {
+        print!("Enter your guess: ");
+        io::stdout().flush().unwrap();
+
+        let mut buffer = String::new();
+
+        match io::stdin().read_line(&mut buffer)
+        {
+            Ok(_) => {
+                match buffer.trim_end().parse::<i64>()
+                {
+                    Ok(guess) => {
+                        if guessing_range.contains(&guess) {
+                            if guess == secret_number {
+                                println!("Correct! You win!");
+                                break;
+                            } else if guess < secret_number {
+                                println!("Too low...");
+                            } else {
+                                println!("Too high...");
+                            }
+                        } else {
+                            if guess < guessing_range.start {
+                                println!("{} is way too low!", guess);
+                            } else {
+                                println!("{} is way too high!", guess);
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        println!("{}", e);
+                    },
+                };
+            },
+            Err(e) => {
+                println!("{}", e);
+            }
+        };
+    }
 }
